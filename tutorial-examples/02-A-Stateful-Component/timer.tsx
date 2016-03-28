2ebba8e8ec9bc6d6a2d9a11557f2a20d94c1f708
TonyPythoneer <tonypythoneer@gmail.com>
2016-03-28 13:44:37 +0800

    Enhance: es6-lize tutorial 02

--- tutorial-examples/02-A-Stateful-Component/timer.tsx
+++ tutorial-examples/02-A-Stateful-Component/timer.tsx
@@ -1,24 +1,27 @@
+/*
+ * Please refer this coding style:
+ *   https://github.com/DefinitelyTyped/DefinitelyTyped/blob/05746119789e2e2ab2206988643657a04229491c/react/react-tests.ts#L435
+ */
 interface State {
     secondsElapsed: number,
 }
 
+
 class Timer extends React.Component<{}, State> {
-    private interval: number;
-    private tick: () => void;
-    constructor() {
-        super()
-        this.state = {
-            secondsElapsed: 0
-        };
-        this.tick = () => {
-            this.setState({ secondsElapsed: this.state.secondsElapsed + 1 });
-        }
+    public state = {
+        secondsElapsed: 0
+    };
+    private _interval: number;
+    tick() {
+        this.setState((prevState, props) => ({
+            secondsElapsed: prevState.secondsElapsed + 1
+        }));
     }
     componentDidMount() {
-        this.interval = setInterval(this.tick, 1000);
+        this._interval = setInterval(() => this.tick(), 1000);
     }
     componentWillUnmount() {
-        clearInterval(this.interval);
+        clearInterval(this._interval);
     }
     render() {
         return (
@@ -26,7 +29,6 @@ class Timer extends React.Component<{}, State> {
         );
     }
 }
-
 
 
 ReactDOM.render(
