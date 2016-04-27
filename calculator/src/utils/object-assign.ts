e4ed8c01ba116fbcdccc12a22240a3e7fcaa861d
TonyPythoneer <tonypythoneer@gmail.com>
2016-04-27 23:59:55 +0800

    Debug: Debug object-assign.ts

--- calculator/src/utils/object-assign.ts
+++ calculator/src/utils/object-assign.ts
@@ -4,32 +4,25 @@
  * so I convert the syntax from es5 to es6.
  * Now it's availble working on tpyescript!
  */
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+const propIsEnumerable = Object.prototype.propertyIsEnumerable;
 
-var hasOwnProperty = Object.prototype.hasOwnProperty;
-var propIsEnumerable = Object.prototype.propertyIsEnumerable;
 
-function toObject(val) {
+function toObject(val): Object {
     if (val === null || val === undefined) {
         throw new TypeError('Object.assign cannot be called with null or undefined');
     }
-
     return Object(val);
 }
 
-export default function(target, source, ...args) {
-    var from;
-    var to = toObject(target);
 
-    for (var s = 1; s < args.length; s++) {
-        from = Object(args[s]);
-
-        for (var key in from) {
-            if (hasOwnProperty.call(from, key)) {
-                to[key] = from[key];
-            }
+export default function(target: Object, ...sources: Object[]) {
+    let to: Object = toObject(target);
+    for (let i: number = 0; i < sources.length; i++) {
+        let s = Object(sources[i]);
+        for (let key in s) {
+            if (hasOwnProperty.call(s, key)) { to[key] = s[key]; }
         }
-
     }
-
     return to;
 };
