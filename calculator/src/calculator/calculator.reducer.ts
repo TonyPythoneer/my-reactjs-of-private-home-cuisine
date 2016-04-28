cd914304dd2a6b8b7f3f81c4af975081558ea371
TonyPythoneer <tonypythoneer@gmail.com>
2016-04-28 20:53:00 +0800

    Enhance: Strength readable

--- calculator/src/calculator/calculator.reducer.ts
+++ calculator/src/calculator/calculator.reducer.ts
@@ -16,47 +16,40 @@ export default function calculator(
     action) {
     switch (action.type) {
         case C.INPUT_NUMBER:
-            let defualtTextNum = action.number === '.' ? '0' : '';
-            let currentTextNum = state.fresh ?
-                defualtTextNum : state.textNum;
-            let newTextNum = currentTextNum + action.number;
-            return objectAssign({}, state, {
-                textNum: newTextNum,
-                fresh: false,
-            })
+            return objectAssign({}, state, inputNumberEvent(state, action));
         case C.INPUT_OPERATOR:
-            let newOp = action.op === '=' ? null: action.op;
-            if (state.regOp) {
-                let result = excuteExpression(state)
-                return objectAssign({}, {
-                    regOp: newOp,
-                    textNum: result,
-                    regNum: result,
-                    fresh: true,
-                })
-            } else {
-                return objectAssign({}, state, {
-                    regOp: newOp,
-                    regNum: state.textNum,
-                    fresh: true,
-                })
-            }
+            return objectAssign({}, state, inputOperatorEvent(state, action));
         case C.CLICK_CLEAN:
-            return objectAssign({}, state, {
-                textNum: "0",
-                fresh: true,
-            })
+            return objectAssign({}, state, { textNum: "0", fresh: true })
         case C.CLICK_ALL_CLEAN:
-            return objectAssign({}, state, {
-                textNum: "0",
-                regNum: 0,
-                textOp: null,
-                regOp: null,
-                fresh: true,
-            })
+            return objectAssign({}, state, initialState)
         default:
             return state;
     }
+}
+
+
+function inputNumberEvent(state, action){
+    // Don't change textNum when monintor is 0 if inputting 0
+    if (state.textNum === "0" && action.number === "0") { return };
+    if (action.number === "." && state.textNum.indexOf('.') !== -1) { return };
+    const defualtTextNum = action.number === '.' ? '0' : '';
+    const currentTextNum = state.fresh ? defualtTextNum : state.textNum;
+    const newTextNum = currentTextNum + action.number;
+    return { textNum: newTextNum, fresh: false }
+}
+
+
+function inputOperatorEvent(state, action) {
+    const regOp = action.op === '=' ? null : action.op;
+    const updateState = { regOp, fresh: true };
+
+    // excute express when operator had existed in state
+    if (state.regOp) {
+        let r = excuteExpression(state);
+        return objectAssign(updateState, { textNum: r, regNum: r });
+    }
+    return objectAssign(updateState, { regNum: state.textNum });
 }
 
 
